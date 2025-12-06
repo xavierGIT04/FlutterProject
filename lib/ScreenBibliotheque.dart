9361515d4e081b2ebf0480e67796fbc1e9bff760
xavier <francoiswakegbandi@gmail.com>
2025-12-06 11:15:07 +0000

    mise en place de la partie onglet de l'écran bibliothèque

--- lib/ScreenBibliotheque.dart
+++ lib/ScreenBibliotheque.dart
@@ -34,9 +34,73 @@ class Screenbibliotheque extends StatelessWidget {
 class _onglets extends StatelessWidget {
   const _onglets({super.key});
 
+  //wigdet personnalisé pour définir le titre de l'onglet et un marqueur(savoir si l'onglet est sélectioné ou pas)
+  Widget tabOnglets(String titreOnglet, bool isSelected){
+      return Padding(
+          padding: EdgeInsetsGeometry.symmetric(horizontal: 8.0),
+          child: Column(
+            children: [
+              Text(
+                titreOnglet,
+                style: TextStyle(
+                  color: isSelected ? Colors.purpleAccent:Colors.white70,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24
+                ),
+              ),
+              //ligne violette qui se trouve en dessous du titre de l'onglet
+              if (isSelected)
+                Container(
+                  color: Colors.purpleAccent,
+                  width: 24,
+                  height: 3,
+                  margin: EdgeInsets.only(top: 4),
+                )
+            ],
+          ),
+      );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: EdgeInsetsGeometry.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              tabOnglets("PlayList", true),
+              tabOnglets("Artiste", false),
+              tabOnglets("Téléchargés", false),
+              Spacer(),
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              OutlinedButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.add, size: 24, color: Colors.white70),
+                  label: Text("créer une playlist", style: TextStyle(color: Colors.white, fontSize: 16),),
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(color: Colors.white70),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadiusGeometry.circular(8)
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10)
+                  ),
+              ),
+              Spacer()
+            ],
+          ),
+          SizedBox(height: 10),
+          Divider(color: Colors.white10, height: 1)
+        ],
+      ),
+
+    );
+
   }
 }
 
